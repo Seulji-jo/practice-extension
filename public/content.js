df7d5ff24cdcd2a6a3a4bb2c8204d1833a36bc73
Seulji-jo <joseuling@gmail.com>
2022-03-23 10:43:33 +0900

    manipulating DOM content(content scripts)

---
+++ public/content.js
@@ -0,0 +1,29 @@
+/*global chrome*/
+const messagesFromAppListener = (message, sender, response) => {
+  console.log('[content.js]. Message received', {message, sender});
+  console.log(chrome.runtime.id);
+  console.log(sender.id);
+
+  if (sender.id === chrome.runtime.id && message.message === 'Hello from React') {
+    const title = document.querySelector('h1');
+    console.log(title);
+    if (title) response(`Title: ${title.textContent}`)
+    else response('No <h1> Title')
+    // response('Hello from content.js')
+  }
+
+  if (sender.id === chrome.runtime.id && message.message === 'delete logo') {
+    // const logo = document.getElementById('hplogo');
+    const logo = document.querySelector('.lnXdpd');
+    if (logo) {
+      logo.parentElement.removeChild(logo);
+      response('구글 로고를 삭제했습니다.')
+    } else {
+      response('구글 로고가 존재하지 않습니다.')
+    }
+  }
+}
+
+// * Fired when a message is sent from either an extension process or a content script.
+chrome.runtime.onMessage.addListener(messagesFromAppListener);
+console.log(chrome.runtime.id);
